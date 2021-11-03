e89ad73644f0aa6b0e5a3598674dbc6feca7c16a
Ismail TAHLI <ismail.tahli@univ-rouen.fr>
2021-11-03 12:19:36 +0000

    Mettre à jour Project/DegreComparator.java

--- Project/DegreComparator.java
+++ Project/DegreComparator.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 public class DegreComparator implements Comparator<Sommet>{
 
 	@Override
-	//On utilisant la compareTo de String
 	public int compare(Sommet arg0, Sommet arg1) {
 		return arg0.getFollowList().size() - arg1.getFollowList().size();
 	}
