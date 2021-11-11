44cd22a0be059b88ca9e57d8a1cf61644aa511c9
Ismail TAHLI <ismail.tahli@univ-rouen.fr>
2021-11-11 19:17:43 +0000

    Update RankComparator.java

--- Project/RankComparator.java
+++ Project/RankComparator.java
@@ -3,6 +3,11 @@ package Project;
 import java.util.Comparator;
 import java.util.Map;
 
+//En peut déclarer cette classe comme anonyme imbriquée dans la classe Graphe
+//Cette classe trie les sommets par PageRank
+//Pour pouvoir trier ces sommets il faut savoir leurs degrés, et du coup la méthode
+//ValueComparator recupére la map des contenant les degré de tous les sommets depuis la classe 
+//Graphe.
 public class RankComparator implements Comparator<Sommet>{
 	private Map<Sommet, Double> degree;
 	//Importer la map contenant les degrees pour pouvoir
@@ -11,10 +16,11 @@ public class RankComparator implements Comparator<Sommet>{
         degree = d;
     }
 	@Override
-	//On utilisant la compareTo de String
+	//On utilisant la compareTo de Double
 	public int compare(Sommet arg0, Sommet arg1) {
+		//La methodes static compareTo de la Classe Double retourne -1,0,1 selon les cas.
 		return degree.get(arg0).compareTo(degree.get(arg1));
 		//return (int) (degree.get(arg0) - degree.get(arg1));
 	}
-	
+	//La je me rends compte qu'il faut quand on bosse un peu plus sur le projet.
 }
